2c09c79b9642482b326c16a3b2343d5bc1a81f1e
silvester <silvesterpang@gmail.com>
2013-07-27 12:41:26 +0800

    init project

---
+++ BSProfileManager/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BSProfileManager
+{
+    public partial class Main : Form
+    {
+        private string versionNumber = "v0.8";
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.Text = string.Format(this.Text, versionNumber);
+        }
+
+    }
+}
